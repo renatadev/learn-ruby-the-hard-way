ad5d1ec5b6a89d112b1cbb8e77696482a40a3327
Renata <renata@jamess-air.home>
2018-11-08 15:13:43 +0000

    edit ex33 and add ex34 to 39

--- ex33.rb
+++ ex33.rb
@@ -1,4 +1,6 @@
 #while loops
+#will execute the code as soon as the expression is TRUE
+#this means, it will run until the expression is FALSE.
 
 i = 0
 numbers = []
@@ -17,10 +19,35 @@ puts "The numbers: "
 #remember you can write this 2 other ways?
 numbers.each {|num| puts num}
 
-#numbers.each do |number|
+# numbers.each do |number|
 #  puts number
+# end
+
+# for number in numbers
+#  puts number
+# end
+
+## STUDY DRILLS
+#Converting the while-loop to a function
+#def while_loop(number)
+#i = 0
+#numbers = []
+
+#while i < number #add parameter - replace num with a variable
+#  puts "At the top i is #{i}"
+#  numbers.push(i)
+
+#  i += 1
+#  puts "Numbers now: ", numbers
+#  puts "At the bottom i is #{i}"
 #end
+
+#puts "The numbers: "
 
 #for number in numbers
 #  puts number
 #end
+#end
+#always remember to end the function!!
+
+#while_loop(3) #call the function adding any param
---
+++ ex34.rb
@@ -0,0 +1,12 @@
+#Accessing elem of arrays
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is the 2nd and is a Ruby"
+puts "The 3rd animal is the 2 and is a peacock"
+puts "The 1st animal is at 0 and is a Bear"
+puts "The animal at 3 is the 4th and is a kangaroo"
+puts "The 5th animal is at 4 and is a whale"
+puts "The animal at 2 is the 3rd and is a peacock"
+puts "The 6th animal is at 5 and is a platypus"
+puts "The animal at 4 is the 5th and is a whale"
